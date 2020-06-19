f8fb7a1ad48db8e50a5eb6584dd4bb0f2788449b
IgorDouven <34210092+IgorDouven@users.noreply.github.com>
2020-06-19 09:31:45 +0200

    Update COVID_PLOT.jl

--- src/COVID_PLOT.jl
+++ src/COVID_PLOT.jl
@@ -16,3 +16,18 @@ function cvd_plot(res::Array{Int64,2}; sird=false)
             lwd = 5)
     end
 end
+
+function cvd_plot(res::Array{Float64,2})
+    l, _ = size(res)
+    p1 = plot(0:l - 1, res[:, 1:4], 
+         label = ["Susceptible" "Infected" "Recovered" "Deceased"], 
+         xlabel = "Time",
+         ylabel = "Count",
+         lwd = 5)
+    p2 = plot(0,l - 1, res[:, 5],
+         xlabel = "Time",
+         ylabel = "Weight",
+         lwd = 5,
+         legend = false)
+    plot(p1, p2, layout = (2, 1))
+end
