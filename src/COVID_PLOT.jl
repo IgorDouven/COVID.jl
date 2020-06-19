c6d17810919d74664dfbff8b6cb24d45646fa167
IgorDouven <34210092+IgorDouven@users.noreply.github.com>
2020-06-19 10:58:17 +0200

    Update COVID_PLOT.jl

--- src/COVID_PLOT.jl
+++ src/COVID_PLOT.jl
@@ -17,9 +17,11 @@ function cvd_plot(res::Array{Int64,2}; sird=false)
     end
 end
 
+#wgt_plt(rs::Array{Float64,2}) = plot(0:size(rs, 1) - 1, rs[:, 5], legend = false, xlabel = "Time", ylabel = "Weight", lwd = 5)
+
 function cvd_plot(res::Array{Float64,2}; sird=false)
     l, _ = size(res)
-    if sird == true
+    p1 = if sird == true
         plot(0:l - 1, res[:, 1:4], 
             label = ["Susceptible" "Infected" "Recovered" "Deceased"], 
             xlabel = "Time",
@@ -34,6 +36,7 @@ function cvd_plot(res::Array{Float64,2}; sird=false)
             ylabel = "Count",
             lwd = 5)
     end
+    p2 = plot(0:l - 1, res[:, 5], legend = false, xlabel = "Time", ylabel = "Weight", lwd = 5)
+    return plot(p1, p2, layout = (2, 1))
 end
 
-wgt_plot(rs::Array{Float64,2}) = plot(0:size(rs, 1) - 1, rs[:, 5], legend=false, xlabel = "Time", ylabel="Weight", lwd = 5)
