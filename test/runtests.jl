b32fe1435fc9655ac2980e7ed101eb1beb5f9071
IgorDouven <34210092+IgorDouven@users.noreply.github.com>
2020-06-22 01:11:26 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 
 @testset "COVID.jl" begin
     pop, gr = create_model(100, .1, 4.)
-    pop[end].pos == 100
-    length(gr) == 1000
+    @test pop[end].pos == 100
+    @test length(gr) == 1000
 end
