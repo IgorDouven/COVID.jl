2f3ac4e044f6d1f391b0378f6f26d0eee04d134f
IgorDouven <34210092+IgorDouven@users.noreply.github.com>
2020-06-21 10:43:53 +0200

    Update COVID_FLEX.jl

--- src/COVID_FLEX.jl
+++ src/COVID_FLEX.jl
@@ -1,7 +1,7 @@
 richard(x::Float64, A::Float64, B::Float64, K::Float64, Q::Float64, ν::Float64) = A + ((K - A) / (1 + Q*exp(-B*x))^(1/ν))
 rich_scaled(x::Float64) = richard(x, .0, 1., .95, 1., 1.) # for the illustrations, we take .9 the highest weight achievable, humanly speaking, and suppose a probability of 10 percent that someone will not leave the house any given day, on the supposition that no constraints are in place, to be realistic
 
-function run_flex(ps::ParameterSetting, scale::Float64, window::Int, numb_updates::Int; init_w=.5)
+function run_model(ps::ParameterSetting, scale::Float64, window::Int, numb_updates::Int; init_w=.5)
     @unpack max_tspan, pr_quick_rec, trans_rate, drop, pr_death, N, λ, hhs = ps
     ppl, grph = create_model(N, λ, hhs)
     s = vcat(dropdims(sum(retrieve_data(ppl), dims=1), dims=1), init_w)
