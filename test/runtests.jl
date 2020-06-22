2f3d90222b96ceb9059d1684c8105459aa6d24fb
IgorDouven <34210092+IgorDouven@users.noreply.github.com>
2020-06-22 11:52:44 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,11 @@ using Test
 
 @testset "COVID.jl" begin
     params = ParameterSetting()
-    res = run_model(params, .4, 20)
-    @test typeof(res) == Array{Int64,2}
+    switch_pts = [(.3, 5), (.9, 5)]
+    res1 = run_model(params, .4, 20)
+    res2 = run_model(params, switch_pts)
+    res3 = run_model(params, .03, 6, 20)
+    @test typeof(res1) == Array{Int64,2}
+    @test typeof(res2) == Array{Int64,2}
+    @test typeof(res3) == Array{Int64,2}
 end
